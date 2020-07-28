82b7c0a82c4236a033700f3d7b8670ca62eb3801
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-07-27 23:30:00 -0400

    Create Name.hpp

---
+++ Name/Name.hpp
@@ -0,0 +1,37 @@
+/*
+name: jianxin zhu
+date: jlu/27/2020
+*/
+
+#include<string>
+
+#ifndef NAME_H
+#define NMAE_H
+
+class Name
+{
+public:
+	Name(const std::string& ="Mr.", const std::string& ="", const std::string& ="", const std::string& = "");
+
+	void set_Name(const std::string&, const std::string&, const std::string&, const std::string&);
+	void set_First_Name(const std::string&);
+	void set_Last_Name(const std::string&);
+	void set_Salutation(const std::string&);
+	void set_Middle_Name(const std::string&);
+
+	std::string get_First_Name() const;
+	std::string get_Last_Name() const;
+	std::string get_Middle_Name() const;
+	std::string get_Salutation() const;
+
+	std::string toString() const;
+
+private:
+	std::string salutation;
+	std::string firstName;
+	std::string lastName;
+	std::string middleName;
+
+
+};
+#endif

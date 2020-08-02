e0a62e6c30809f2eefec9cca821b4d823dd3561a
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-08-01 21:54:21 -0400

    Create MainFunction.cpp

---
+++ Time/MainFunction.cpp
@@ -0,0 +1,16 @@
+/*
+name: jianxin zhu
+date: aug/01/2020
+*/
+
+#include<iostream>
+#include"Time.h"
+
+using namespace std;
+
+int main()
+{
+	Time t1{3,30,30};
+
+	cout << t1.toUniversalString();
+}
